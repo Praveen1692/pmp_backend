952374378855842cc3bf284f7ea8f499fc555e68
Praveen Sharma <praying.sharma@gmail.com>
2025-09-05 13:08:04 +0530

    Async Handler Function

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -1,11 +1,18 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheak = (req, res) => {
-  try {
-    return res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is running.." }));
-  } catch (error) {}
-};
+// const healthCheak = (req, res) => {
+//   try {
+//     return res
+//       .status(200)
+//       .json(new ApiResponse(200, { message: "Server is running.." }));
+//   } catch (error) {}
+// };
+
+const healthCheak = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, { message: "Server is running..." }));
+});
 
 export { healthCheak };
