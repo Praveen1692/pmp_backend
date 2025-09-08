cc929246b97022337faad370a5993baae313751a
Praveen Sharma <praying.sharma@gmail.com>
2025-09-08 15:09:03 +0530

    Password Hash

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -56,16 +57,16 @@ const userSchema = new mongoose.Schema(
     emailverificationExpiry: {
       type: Date,
     },
-
   },
   {
     timestamps: true,
   },
 );
 
-
-
-
-
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
